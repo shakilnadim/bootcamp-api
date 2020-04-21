f5c96fbb119f1be85a3d99490e8ca0078c7b8b42
Nadim Shakil <shakilnadim@gmail.com>
2020-04-21 14:09:59 +0600

    Populate courses with bootcamps, add vairtuals in bootcamp for courses and cascade delete courses for bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,7 +29,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // Finding resources
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select fields
   if (req.query.select) {
@@ -90,7 +90,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
  * @access  Public
  */
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id).populate('courses');
 
   if (!bootcamp) {
     return next(
@@ -147,7 +147,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
  * @access  Private
  */
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
@@ -155,25 +155,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
 
-  res.status(200).json({
-    success: true,
-    data: bootcamp,
-  });
-});
-
-/**
- * @desc    Deletes bootcamp
- * @route   DELETE /api/v1/bootcamps/:id
- * @access  Private
- */
-exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-
-  if (!bootcamp) {
-    return next(
-      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-    );
-  }
+  bootcamp.remove();
 
   res.status(200).json({
     success: true,
